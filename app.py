7162c52c1ace00aad881ea2701350eecd143bccd
Prateeksha-Gangwar <prateekshaganwar1@gmail.com>
2026-06-30 13:52:46 +0530

    dream big

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ def greet():
     print("Hello, Nodejs Developer-1!")
     print("Hello, Python Developer-2!")
     print("Hello, PHP Developer-2!"
-hello From Backend Developer
+hello From Frontend Developer
 HELLO DEVOPS ENGINEER
+          Dream Big
 greet()
 Ranjit Sir & ritik pati
